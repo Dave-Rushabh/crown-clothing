c7b0cbf294ed99da83e8f0ef4e2e237053548ea2
dave-rushabh <rushabh.dave24@gmail.com>
2022-05-29 12:44:21 +0530

    Primary structure of website completed

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,19 +1,26 @@
-import "./DirectoryItem.scss";
+import { useNavigate } from "react-router-dom";
+
+import {
+  BackgroundImage,
+  Body,
+  DirectoryContainer,
+} from "./DirectoryItemStyle";
 
 const DirectoryItem = ({ categoryDetails }) => {
-  const { imageUrl, title } = categoryDetails;
+  const { imageUrl, title, route } = categoryDetails;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
     <>
-      <div className="directory-container">
-        <div
-          className="background-image"
-          style={{ backgroundImage: `url(${imageUrl})` }}
-        />
-        <div className="body">
+      <DirectoryContainer onClick={onNavigateHandler}>
+        <BackgroundImage imageUrl={imageUrl} />
+        {/* Styled components can accept props, here imageUrl is a prop */}
+        <Body>
           <h2> {title}</h2>
           <p>Shop Now</p>
-        </div>
-      </div>
+        </Body>
+      </DirectoryContainer>
     </>
   );
 };
--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -7,28 +7,35 @@ const Directory = () => {
       id: 1,
       title: "Hats",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      route: "shop/hats",
     },
     {
       id: 2,
       title: "Jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      route: "shop/jackets",
     },
     {
       id: 3,
       title: "Sneakers",
       imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      route: "shop/sneakers",
     },
     {
       id: 4,
       title: "Women's",
       imageUrl:
         "https://images.unsplash.com/photo-1483985988355-763728e1935b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+
+      route: "shop/womens",
     },
     {
       id: 5,
       title: "Men's",
       imageUrl:
         "https://images.unsplash.com/photo-1487744480471-9ca1bca6fb7d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1191",
+
+      route: "shop/mens",
     },
   ];
 
